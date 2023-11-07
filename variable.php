b0387a701e5703523bbc282497005439e784f89c
bigkung66-2 <sudthipat190@gmail.com>
2023-11-07 10:44:05 +0700

    variable text

---
+++ variable.php
@@ -0,0 +1,19 @@
+<?php
+$name = "Sudthipat";
+$department = "Business Computer";
+
+echo $name . " " . $department . "<br>";
+
+$num1 = 100;
+$num2 = 200;
+$num3 = 300;
+$num4 = 400;
+
+$sum = $num1 + $num2 + $num3 + $num4;
+echo "sum = " . $sum . "<br>";
+$avg = $sum / 4;
+echo "avg = " . $avg;
+
+
+// ถ้าใช้งาน php ร่วมกันกับ html ไม่ใส่ tag ปิดก็ได้
+?>
